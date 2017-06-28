dd755ab20d42f95f7ceb8f2ffac33de22f5697ff
Sasha Pavlov <xanbimei@yandex.ru>
2017-06-28 10:20:06 +0300

    comment test

--- promise.js
+++ promise.js
@@ -1,40 +1,16 @@
 'use strict';
 
-function Promise (func) {
-    const self = this;
+function Promise () {
 
-    function resolve(...smth) {
-        self.result = smth;
-        if(self.func) {
-            self.func(smth);
-        }
-    }
-
-    function reject(err) {
-        self.err = err;
-        if(self.errFunc) {
-            self.errFunc(err);
-        }
-    }
-
-    func(resolve, reject);
 }
 
 Promise.prototype = {
-    then: function (func) {
-        if(this.result) {
-            return func(...this.result);
-        } else {
-            this.func = func;
-        }
-        return this;
+    then: function () {
+
+
     },
-    catch: function (func) {
-        if(this.err) {
-            func(this.err);
-        } else {
-            this.errFunc = func;
-        }
+    catch: function () {
+
     }
 };
 
