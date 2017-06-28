429ba3c61dc42c4031f1427e00f9f629174ff86a
Sasha Pavlov <xanbimei@yandex.ru>
2017-06-28 10:32:24 +0300

    uncomment

--- promise.js
+++ promise.js
@@ -1,16 +1,40 @@
 'use strict';
 
-function Promise () {
+function Promise (func) {
+    const self = this;
 
+    function resolve(...smth) {
+        self.result = smth;
+        if(self.func) {
+            self.func(smth);
+        }
+    }
+
+    function reject(err) {
+        self.err = err;
+        if(self.errFunc) {
+            self.errFunc(err);
+        }
+    }
+
+    func(resolve, reject);
 }
 
 Promise.prototype = {
-    then: function () {
-
-
+    then: function (func) {
+        if(this.result) {
+            return func(...this.result);
+        } else {
+            this.func = func;
+        }
+        return this;
     },
-    catch: function () {
-
+    catch: function (func) {
+        if(this.err) {
+            func(this.err);
+        } else {
+            this.errFunc = func;
+        }
     }
 };
 
